bcc378be8c02e4ec310a4a784fbdeda2c232c475
Satyaprakash Kushwaha <satyaprakash.kushwaha@edobo.co>
2024-10-23 10:38:37 +0530

    authentication.js

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -1,30 +1,29 @@
-const { default: axios } = require('axios');
+const { default: axios } = require("axios");
 
 async function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
-    if (!token) return res.sendStatus(401);
+  if (!token) return res.sendStatus(401);
 
-    try {
-        // Validate token by making a request to the PHP backend
-        const response = await axios.get('http://127.0.0.1:8000/api/validate-token', {
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        });
+  try {
+    // Validate token by making a request to the PHP backend
+    const response = await axios.get("http://13.61.33.202/api/validate-token", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-        if (response.status === 200) {
-            req.user = response.data.user;
-            next();
-        } else {
-            res.sendStatus(response.status);
-        }
-    } catch (error) {
-        console.log(error.message);
-        res.sendStatus(403);
+    if (response.status === 200) {
+      req.user = response.data.user;
+      next();
+    } else {
+      res.sendStatus(response.status);
     }
+  } catch (error) {
+    console.log(error.message);
+    res.sendStatus(403);
+  }
 }
 
-
-module.exports = { authenticateToken }
+module.exports = { authenticateToken };
